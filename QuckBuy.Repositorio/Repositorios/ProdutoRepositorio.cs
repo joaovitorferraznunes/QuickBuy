9d886ad76a0190b8e5e1005ea78431e43cd7e1f6
joaovitorferraznunes <59182990+joaovitorferraznunes@users.noreply.github.com>
2019-12-23 23:31:59 -0300

    aula 01

--- QuckBuy.Repositorio/Repositorios/ProdutoRepositorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using QuickBuy.Dominio.Contratos;
-using QuickBuy.Dominio.Entidades;
-
-namespace QuckBuy.Repositorio.Repositorios
-{
-    public class ProdutoRepositorio: BaseRepositorio<Produto>, IProdutoRepositorio
-    {
-    }
-}
--- QuickBuy/Contratos/IDiposable.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace QuickBuy.Dominio.Contratos
-{
-    public interface IDiposable
-    {
-    }
-}
