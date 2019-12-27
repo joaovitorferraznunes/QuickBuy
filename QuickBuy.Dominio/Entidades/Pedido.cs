18934d52889b2b54e000deabeb0b10a1d00995f5
joaovitorferraznunes <59182990+joaovitorferraznunes@users.noreply.github.com>
2019-12-26 21:49:12 -0300

    dando classes à pasta entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+     public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um pedido
+        /// ou muitos pedidos
+        /// </summary>
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+    }
+}
