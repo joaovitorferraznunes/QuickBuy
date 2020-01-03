68f6c1cefed2be5b3ee3555ba471df9b84b6ad1f
joaovitorferraznunes <59182990+joaovitorferraznunes@users.noreply.github.com>
2020-01-03 12:12:39 -0300

    terminando classes: pedido entidade e produto

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,7 +1,13 @@
-﻿namespace QuickBuy.Dominio.Entidades
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
-  
+        public List<string> _mensagensValidacao { get; set; }
+        public List<string> MensagemValidacao
+        {
+         get { return _mensagensValidacao ?? (_mensagensValidacao = new List<string>()); }
+        }
     }
 }
--- QuickBuy.Dominio/Entidades/ItemPedido.cs
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuickBuy.Dominio.Entidades
 {
-    public class ItemPedido
+    public class ItemPedido : Entidade
     {
         public int Id { get; set; }
         public int ProdutoId { get; set; }
