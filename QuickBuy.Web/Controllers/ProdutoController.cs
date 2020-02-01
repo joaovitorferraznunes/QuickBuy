568400a19ae6bd640e739ac22031ce39efa84f91
joaovitorferraznunes <59182990+joaovitorferraznunes@users.noreply.github.com>
2020-02-01 13:31:34 -0300

    Adicionando o Contexto ao Repositório

---
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+using QuickBuy.Repositorio.Repositorios;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = new ProdutoRepositorio()
+            _produtoRepositorio = produtoRepositorio; 
+        }
+    }
+}
