17e84c98f222cde264d659493ef85d23a829646a
joaovitorferraznunes <59182990+joaovitorferraznunes@users.noreply.github.com>
2020-02-07 09:43:19 -0300

    mandando para o post man

--- QuickBuy.Web/Controllers/ProdutoController.cs
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
 using QuickBuy.Repositorio.Repositorios;
+using System;
 
 namespace QuickBuy.Web.Controllers
 {
@@ -10,8 +12,37 @@ namespace QuickBuy.Web.Controllers
         private readonly IProdutoRepositorio _produtoRepositorio;
         public ProdutoController(IProdutoRepositorio produtoRepositorio)
         {
-            _produtoRepositorio = new ProdutoRepositorio()
             _produtoRepositorio = produtoRepositorio; 
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos());
+                //if(condicao == false)
+                //{
+                //  return BadRequest("")
+                //}
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto)
+        {
+            try
+            {
+                _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
         }
     }
 }
